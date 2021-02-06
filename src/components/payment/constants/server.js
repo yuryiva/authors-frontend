8ebd3c72fa36ca23ce0bcfdd3c953fff6ed39539
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-06 23:03:34 +0300

    authors2 in server.js

--- src/components/payment/constants/server.js
+++ src/components/payment/constants/server.js
@@ -1,16 +1,6 @@
-// const PAYMENT_SERVER_URL =
-//   process.env.NODE_ENV === "production"
-//     ? "https://the-authors2.herokuapp.com"
-//     : "https://the-authors2.herokuapp.com";
-
-// export default PAYMENT_SERVER_URL;
-
-
-
 const PAYMENT_SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? "http://myapidomain.com"
-    // : "http://localhost:8080";
+    ? "https://the-authors2.herokuapp.com"
     : "https://the-authors2.herokuapp.com";
 
 export default PAYMENT_SERVER_URL;

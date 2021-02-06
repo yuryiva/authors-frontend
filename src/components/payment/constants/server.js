59b8d35d7c8d215dfd650d521a5d477189c69053
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-06 22:56:11 +0300

    returned payment/constants/server.js to main in 1 version

--- src/components/payment/constants/server.js
+++ src/components/payment/constants/server.js
@@ -1,6 +1,17 @@
+// const PAYMENT_SERVER_URL =
+//   process.env.NODE_ENV === "production"
+//     ? "https://the-authors2.herokuapp.com"
+//     : "https://the-authors2.herokuapp.com";
+
+// export default PAYMENT_SERVER_URL;
+
+
+
 const PAYMENT_SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? "https://the-authors2.herokuapp.com"
+    ? "http://myapidomain.com"
+    // : "http://localhost:8080";
     : "https://the-authors2.herokuapp.com";
 
 export default PAYMENT_SERVER_URL;
+

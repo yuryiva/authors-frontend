342f2b8bcb26c9b3895eefddd637f4c3bc2a6827
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-06 22:50:08 +0300

    returned payment/constants/server.js to previous condition

--- src/components/payment/constants/server.js
+++ src/components/payment/constants/server.js
@@ -1,7 +1,6 @@
 const PAYMENT_SERVER_URL =
   process.env.NODE_ENV === "production"
-    
-    ? "https://localhost:8080"
-    :"https://the-authors2.herokuapp.com"
+    ? "https://the-authors2.herokuapp.com"
+    : "https://the-authors2.herokuapp.com";
 
 export default PAYMENT_SERVER_URL;

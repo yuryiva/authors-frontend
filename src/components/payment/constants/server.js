f7db220484074da7d7e5f490d9d386d0377ee336
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-06 22:34:20 +0300

    modified payment/constants/server.js with ternary operator from context

--- src/components/payment/constants/server.js
+++ src/components/payment/constants/server.js
@@ -1,10 +1,17 @@
-
 const PAYMENT_SERVER_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://the-authors2.herokuapp.com"
-    : "https://the-authors2.herokuapp.com";
+context.state.productionOrDevelopment === "production"
+? `https://the-authors2.herokuapp.com/preorder`
+: `http://localhost:8080/preorder`
 
 export default PAYMENT_SERVER_URL;
+
+
+// const PAYMENT_SERVER_URL =
+//   process.env.NODE_ENV === "production"
+//     ? "https://the-authors2.herokuapp.com"
+//     : "https://the-authors2.herokuapp.com";
+
+// export default PAYMENT_SERVER_URL;
 
 
 

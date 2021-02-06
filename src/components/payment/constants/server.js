70d63c608e802ecccf3757466268f79f86af2553
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-06 22:40:07 +0300

    modified payment/constants/server.js with ternary operator from context3

--- src/components/payment/constants/server.js
+++ src/components/payment/constants/server.js
@@ -1,15 +1,14 @@
 import { Context } from "../../../context/Context";
 import { useContext } from "react";
 
-const context = useContext(Context)
-
-const PAYMENT_SERVER_URL =
-context.state.productionOrDevelopment === "production"
-? `https://the-authors2.herokuapp.com/preorder`
-: `http://localhost:8080/preorder`
+const PAYMENT_SERVER_URL = () => {
+  const context = useContext(Context);
+  context.state.productionOrDevelopment === "production"
+    ? `https://the-authors2.herokuapp.com/preorder`
+    : `http://localhost:8080/preorder`;
+};
 
 export default PAYMENT_SERVER_URL;
-
 
 // const PAYMENT_SERVER_URL =
 //   process.env.NODE_ENV === "production"
@@ -17,6 +16,3 @@ export default PAYMENT_SERVER_URL;
 //     : "https://the-authors2.herokuapp.com";
 
 // export default PAYMENT_SERVER_URL;
-
-
-

a59276f844f4bf32e2ae2880fefe438e4a39acef
Yury Ivashchanka <yury.ivashchanka@yahoo.com>
2021-02-25 22:01:44 +0300

    added underline and zoom in for titles on landing page

--- src/components/landingPage/OurImpact.js
+++ src/components/landingPage/OurImpact.js
@@ -9,7 +9,9 @@ export default function OurImpact() {
   return (
     <AboutUsSection>
       <MainHeading>
-        <h3>Who We Are</h3>
+        <Link style={{ textDecoration: "none", color: "black" }} to="our-team">
+          <h3>Who We Are</h3>
+        </Link>
         <p>
           Contrary to popular belief, Lorem Ipsum is not simply random text. It
           has roots in a piece of classical Latin literature from 45 BC, making
@@ -25,15 +27,15 @@ export default function OurImpact() {
             style={{ textDecoration: "none", color: "inherit" }}
           >
             <h4>How we work</h4>
-            <div className="underbar"></div>
-            <p>
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC,
-              making it over 2000 years old. Richard McClintock, a Latin
-              professor at Hampden-Sydney College in Virginia, looked up one of
-              the more obscure Latin words.
-            </p>
           </Link>
+          {/* <div className="underbar"></div> */}
+          <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words.
+          </p>
         </OurImpactDescription>
         <OurImpactPhoto>
           <img src={ourImpactPhoto} alt="pen" />
@@ -78,7 +80,13 @@ const MainHeading = styled.div`
     border-top: 4px solid #000;
     width: 177px;
     margin: 50px auto;
+    text-align: center;
     /* margin-bottom: 0; */
+    &:hover {
+      cursor: pointer;
+      transition: transform 0.5s;
+      transform: scale(1.05);
+    }
   }
 
   p {
@@ -114,22 +122,22 @@ const OurImpactDescription = styled.section`
   justify-content: center;
   align-items: center;
 
-  underbar {
+  /* .underbar {
     width: 0;
     height: 5px;
-    ${"" /* background: rgba(0, 0, 0, 0); */}
+    background: rgba(0, 0, 0, 0);
     background: rgba(100, 100, 200, 0);
     top: 20px;
     position: absolute;
     -webkit-transition: 0.5s ease;
-  }
+  } */
 
   h4 {
     font-size: 1.5rem;
     margin-bottom: 20px;
     padding: 0 25px;
     text-align: center;
-    -webkit-transition: 0.5s ease;
+    /* -webkit-transition: 0.5s ease;
 
     &:hover {
       text-shadow: 0px -2px 0px rgba(0, 0, 0, 1),
@@ -142,9 +150,16 @@ const OurImpactDescription = styled.section`
       -webkit-transition: 2.5s ease;
 
       background: black;
-      ${"" /* background: rgba(150, 250, 250, 0.5); */}
-      ${"" /* box-shadow: grey; */}
+      background: rgba(150, 250, 250, 0.5);
+      box-shadow: grey;
       box-shadow: 0 0 10px rgba(70, 100, 150, 4);
+    } */
+
+    text-decoration: underline;
+    &:hover {
+      cursor: pointer;
+      transition: transform 0.5s;
+      transform: scale(1.05);
     }
   }
 
@@ -221,7 +236,7 @@ const OurValuesDescription = styled.section`
     text-decoration: underline;
     &:hover {
       cursor: pointer;
-      transition: transform 0.3s;
+      transition: transform 0.5s;
       transform: scale(1.05);
     }
   }

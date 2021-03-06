3c58c8fcb2bdaaf98cd4fd6830921932dc0a4373
Yury Ivashchanka <72413479+yuryiva@users.noreply.github.com>
2021-03-06 20:06:57 +0300

    added https://authxrs.herokuapp.com
    added https://authxrs.herokuapp.com to constants/server.js

--- src/components/payment/constants/server.js
+++ src/components/payment/constants/server.js
@@ -1,7 +1,7 @@
 const PAYMENT_SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? "https://the-authors2.herokuapp.com"
-    : "https://the-authors2.herokuapp.com"
+    ? "https://authxrs.herokuapp.com"
+    : "https://authxrs.herokuapp.com"
     // ? "http://localhost:8080"
     // : "http://localhost:8080";
 
